4d9b710a5ee9920bcc0764e404376ba4dcf518c6
Nick Molargik <nick_molargik@sweetwater.com>
2023-01-16 10:08:03 -0500

    updated to support apple watch app installation -> 2.4.2

---
+++ FWPD Scanner Wrist Watch App/FWPD_Scanner_WristApp.swift
@@ -0,0 +1,17 @@
+//
+//  FWPD_Scanner_WristApp.swift
+//  FWPD Scanner Wrist Watch App
+//
+//  Created by Nick Molargik on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FWPD_Scanner_Wrist_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WatchMainView()
+        }
+    }
+}
